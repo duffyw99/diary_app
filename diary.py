a3ea604761f2b4d7d2d51b55e95c77223ccd1e84
duffyw99 <mduffyw99@gmail.com>
2016-08-07 22:22:43 -0400

    added functions to delete entries and clear the screen between menu choices

--- diary.py
+++ diary.py
@@ -2,6 +2,7 @@
 
 from collections import OrderedDict
 import datetime
+import os
 import sys
 
 from peewee import *
@@ -28,6 +29,7 @@ def menu_loop(self):
         choice = input('Action: '.lower().strip()
 
         if choice in menu:
+            clear()
             # return thier choice and run it with: ()
             menu[choice]()
 
@@ -35,6 +37,9 @@ def initialize(self):
     """Create the database and the table if they don't exist"""
     db.conect()
     db.create_tables([Entry], safe=True)
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def add_entry(self):
     """add an entry"""
@@ -58,22 +63,30 @@ def view_entries(search_query=None):
 
     for entry in entries:
         timestamp = Entry.timestamp.strftime('%A %B %d, %Y %I:%M%p')
+        clear()
         print(timestamp)
         print('='*len(timestamp))
         print(entry.content)
+        print('/n/n'+'=' * len(timestamp))
         print('n) next entry.')
+        print('d) delete this entry.')
         print('q) back to main menu.')
 
-        next_action = input('Action: [Nq]').lower().strip()
+        next_action = input('Action: [Ndq]').lower().strip()
         if next_action == 'q':
             break
+        elif next_action == 'd':
+            delete_entry(entry)
 
 def search_entries():
     """Search entries for a keyword"""
     view_entries(input('Search for keyword: '))
 
-def delete_entry(self):
-    #delete an entry
+def delete_entry(entry):
+    """delete an entry"""
+    if input('Are you sure? [yN]').lower() == 'y':
+        entry.delete_instance()
+        print('Entry deleted!')
 
 menu = OrderedDict([
     ('a': add_entry),
